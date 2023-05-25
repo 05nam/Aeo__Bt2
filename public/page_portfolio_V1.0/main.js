5d2021746fecf053f9acf118b277ce0a28e890c4
namkagawa <namk14bk@gmail.com>
2023-05-25 21:55:34 +0700

    g

--- public/page_portfolio_V1.0/main.js
+++ public/page_portfolio_V1.0/main.js
@@ -6,7 +6,31 @@ $(document).ready(function () {
             // autoplaySpeed: 2000,
             // Gắn sự kiện click cho nút điều hướng tùy chỉnh
             prevArrow: "<button type='button' class='slick-prev pull-left slick--arrow'><i class='bx bxs-right-arrow' aria-hidden='true'></i></button>",
-            nextArrow: "<button type='button' class='slick-next pull-right slick--arrow'><i class='bx bxs-left-arrow' aria-hidden='true'></i></button>"
+            nextArrow: "<button type='button' class='slick-next pull-right slick--arrow'><i class='bx bxs-left-arrow' aria-hidden='true'></i></button>",
+
+
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 2,
+                        //   slidesToScroll: 3,
+                        infinite: true,
+                        dots: false,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        //   slidesToScroll: 3,
+                        infinite: true,
+                        dots: false,
+                        arrows: false,
+                    }
+                },
+            ]
         }
     );
 
